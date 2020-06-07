d7d2329419241ca8ae2ed2917c7bbacb08cd8a00
georg3103 <gnmkrtchyan@gmail.com>
2020-06-07 21:54:34 +0300

    fix(Messages): horizontall scroll

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -15,10 +15,12 @@ const Messages = () => {
       {messages.map(({
         id, message, user, date,
       }) => (
-        <div key={id}>
-          <div><b>{user}</b></div>
-          <p className="mb-0">{message}</p>
-          <p className="mb-2"><small className="text-muted">{date}</small></p>
+        <div key={id} className="card mb-4 w-100 border-0">
+          <div className="col-sm-12">
+            <div><b>{user}</b></div>
+            <p className="mb-0">{message}</p>
+            <p className="mb-2"><small className="text-muted">{date}</small></p>
+          </div>
         </div>
       ))}
     </div>
