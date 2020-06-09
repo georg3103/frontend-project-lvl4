2a6b4d27021a50db066396fa0b33cfff649ef000
georg3103 <gnmkrtchyan@gmail.com>
2020-06-09 17:08:41 +0300

    feat(routes): added path

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 // @ts-check
+import path from 'path';
 
 const host = '';
 const prefix = 'api/v1';
 
 export default {
-  channelsPath: () => [host, prefix, 'channels'].join('/'),
-  channelPath: (id) => [host, prefix, 'channels', id].join('/'),
-  channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
+  channelsPath: () => path.join(host, prefix, 'channels'),
+  channelPath: (id) => path.join(host, prefix, 'channels', String(id)),
+  channelMessagesPath: (id) => path.join(host, prefix, 'channels', String(id), 'messages'),
+
 };
